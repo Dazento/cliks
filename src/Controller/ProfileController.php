32135d32393f936885bf88349f179a16b3b3378f
DylanB <dylanbury.pro@gmail.com>
2022-09-26 10:43:40 +0200

    crud profile ended

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\UserAdress;
+use App\Form\UserType;
 use App\Service\UserAdressService;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/profile', name: 'profile')]
 class ProfileController extends AbstractController
@@ -44,7 +44,26 @@ class ProfileController extends AbstractController
     public function deleteAdress(UserAdressService $userService, UserAdress $userAdress): Response
     {
         $userService->delete($userAdress);
-        
+
         return $this->redirectToRoute('profile_index');
+    }
+
+    #[Route('/modifier-informations', name: '_user_edit')]
+    public function editInfos(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $user = $this->getUser();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $manager = $managerRegistry->getManager();
+            $user = $userForm->getData();
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('profile_index');
+        }
+        return $this->render('profile/editUser.html.twig', [
+            'userForm' => $userForm->createView(),
+        ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'minLength' => 3,
+                    'maxLength' => 50,
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'minLength' => 3,
+                    'maxLength' => 50,
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
