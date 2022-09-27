9c1aba5aafef2b0e4623d7098a969ffbe61b82ad
DylanB <dylanbury.pro@gmail.com>
2022-09-27 17:19:31 +0200

    home responsive

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -67,8 +67,8 @@ class RegistrationFormType extends AbstractType
                         new PasswordStrength([
                             'minLength' => 8,
                             'tooShortMessage' => 'Le mot de passe doit contenir au moins 8 caractères.',
-                            'minStrength' => 4,
-                            'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.'
+                            'minStrength' => 3,
+                            'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule et un chiffre.'
                         ])
                     ],
                 ],
