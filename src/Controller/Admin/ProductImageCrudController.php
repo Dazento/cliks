219d3eb64ec6c6e29324d698467b32d78bfb4a06
DylanB <dylanbury.pro@gmail.com>
2022-10-03 12:14:53 +0200

    products page optimized and composer updated

--- src/Controller/Admin/ProductImageCrudController.php
+++ src/Controller/Admin/ProductImageCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\ProductImage;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -19,6 +20,7 @@ class ProductImageCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id')->hideOnForm(),
             ImageField::new('image')
                 ->onlyOnIndex()
                 ->setBasePath("/uploads/product"),
